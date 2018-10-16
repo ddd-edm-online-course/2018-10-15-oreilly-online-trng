ef35f4d7252a778295c31c2120d9e94ef4731687
Matt Stine <mstine@pivotal.io>
2018-10-16 14:43:35 -0500

    [Lab 6] submission

--- src/main/java/com/mattstine/dddworkshop/pizzashop/kitchen/InProcessEventSourcedPizzaRepository.java
+++ src/main/java/com/mattstine/dddworkshop/pizzashop/kitchen/InProcessEventSourcedPizzaRepository.java
@@ -12,26 +12,26 @@ import java.util.stream.Collectors;
 
 final class InProcessEventSourcedPizzaRepository extends InProcessEventSourcedRepository<PizzaRef, Pizza, Pizza.PizzaState, PizzaEvent, PizzaAddedEvent> implements PizzaRepository {
 
-    Map<KitchenOrderRef, Set<PizzaRef>> kitchenOrderRefToPizzaSet;
+    Map<KitchenOrderRef, Set<PizzaRef>> kitchenOrderRefToPizzaRefSet;
 
     InProcessEventSourcedPizzaRepository(EventLog eventLog, Topic pizzas) {
         super(eventLog, PizzaRef.class, Pizza.class, Pizza.PizzaState.class, PizzaAddedEvent.class, pizzas);
 
-        kitchenOrderRefToPizzaSet = new HashMap<>();
+        kitchenOrderRefToPizzaRefSet = new HashMap<>();
 
         eventLog.subscribe(new Topic("pizzas"), e -> {
             if (e instanceof PizzaAddedEvent) {
                 PizzaAddedEvent pae = (PizzaAddedEvent) e;
-                KitchenOrderRef kitchenOrderRef = pae.getState().getKitchenOrderRef();
-                Set<PizzaRef> pizzaSet = kitchenOrderRefToPizzaSet.computeIfAbsent(kitchenOrderRef, k -> new HashSet<>());
-                pizzaSet.add(pae.getRef());
+                Set<PizzaRef> pizzaRefs = kitchenOrderRefToPizzaRefSet.computeIfAbsent(pae.getState().getKitchenOrderRef(), k -> new HashSet<>());
+                pizzaRefs.add(pae.getRef());
             }
         });
     }
 
     @Override
     public Set<Pizza> findPizzasByKitchenOrderRef(KitchenOrderRef kitchenOrderRef) {
-        return kitchenOrderRefToPizzaSet.get(kitchenOrderRef)
-                .stream().map(this::findByRef).collect(Collectors.toSet());
+        return kitchenOrderRefToPizzaRefSet.get(kitchenOrderRef).stream()
+                .map(this::findByRef)
+                .collect(Collectors.toSet());
     }
 }
