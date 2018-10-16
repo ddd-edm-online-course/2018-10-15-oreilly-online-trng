f1cd7def510b4f77d376af5e17f03a446d1d7aad
Matt Stine <mstine@pivotal.io>
2018-10-16 13:56:16 -0500

    [Lab 5] submission

--- src/main/java/com/mattstine/dddworkshop/pizzashop/kitchen/InProcessEventSourcedKitchenOrderRepository.java
+++ src/main/java/com/mattstine/dddworkshop/pizzashop/kitchen/InProcessEventSourcedKitchenOrderRepository.java
@@ -10,6 +10,8 @@ import java.util.Map;
 
 final class InProcessEventSourcedKitchenOrderRepository extends InProcessEventSourcedRepository<KitchenOrderRef, KitchenOrder, KitchenOrder.OrderState, KitchenOrderEvent, KitchenOrderAddedEvent> implements KitchenOrderRepository {
 
+    Map<OnlineOrderRef, KitchenOrderRef> onlineOrderRefToKitchenOrderRef;
+
     InProcessEventSourcedKitchenOrderRepository(EventLog eventLog, Topic topic) {
         super(eventLog,
                 KitchenOrderRef.class,
@@ -17,10 +19,23 @@ final class InProcessEventSourcedKitchenOrderRepository extends InProcessEventSo
                 KitchenOrder.OrderState.class,
                 KitchenOrderAddedEvent.class,
                 topic);
+
+        onlineOrderRefToKitchenOrderRef = new HashMap<>();
+
+        eventLog.subscribe(new Topic("kitchen_orders"), e -> {
+            if (e instanceof KitchenOrderAddedEvent) {
+                KitchenOrderAddedEvent koae = (KitchenOrderAddedEvent) e;
+                onlineOrderRefToKitchenOrderRef.put(koae.getState().getOnlineOrderRef(), koae.getRef());
+            }
+        });
     }
 
     @Override
     public KitchenOrder findByOnlineOrderRef(OnlineOrderRef onlineOrderRef) {
+        KitchenOrderRef kitchenOrderRef = onlineOrderRefToKitchenOrderRef.get(onlineOrderRef);
+        if (kitchenOrderRef != null) {
+            return this.findByRef(kitchenOrderRef);
+        }
         return null;
     }
 }
--- src/main/java/com/mattstine/dddworkshop/pizzashop/kitchen/InProcessEventSourcedPizzaRepository.java
+++ src/main/java/com/mattstine/dddworkshop/pizzashop/kitchen/InProcessEventSourcedPizzaRepository.java
@@ -12,12 +12,26 @@ import java.util.stream.Collectors;
 
 final class InProcessEventSourcedPizzaRepository extends InProcessEventSourcedRepository<PizzaRef, Pizza, Pizza.PizzaState, PizzaEvent, PizzaAddedEvent> implements PizzaRepository {
 
+    Map<KitchenOrderRef, Set<PizzaRef>> kitchenOrderRefToPizzaSet;
+
     InProcessEventSourcedPizzaRepository(EventLog eventLog, Topic pizzas) {
         super(eventLog, PizzaRef.class, Pizza.class, Pizza.PizzaState.class, PizzaAddedEvent.class, pizzas);
+
+        kitchenOrderRefToPizzaSet = new HashMap<>();
+
+        eventLog.subscribe(new Topic("pizzas"), e -> {
+            if (e instanceof PizzaAddedEvent) {
+                PizzaAddedEvent pae = (PizzaAddedEvent) e;
+                KitchenOrderRef kitchenOrderRef = pae.getState().getKitchenOrderRef();
+                Set<PizzaRef> pizzaSet = kitchenOrderRefToPizzaSet.computeIfAbsent(kitchenOrderRef, k -> new HashSet<>());
+                pizzaSet.add(pae.getRef());
+            }
+        });
     }
 
     @Override
     public Set<Pizza> findPizzasByKitchenOrderRef(KitchenOrderRef kitchenOrderRef) {
-        return null;
+        return kitchenOrderRefToPizzaSet.get(kitchenOrderRef)
+                .stream().map(this::findByRef).collect(Collectors.toSet());
     }
 }
